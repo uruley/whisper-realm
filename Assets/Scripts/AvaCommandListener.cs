a2dff270bc2fc9d956285fac35a200f6922bc6e9
uruley <166458385+uruley@users.noreply.github.com>
2025-04-05 10:16:58 -0500

    Update AvaCommandListener.cs

--- Assets/Scripts/AvaCommandListener.cs
+++ Assets/Scripts/AvaCommandListener.cs
@@ -1,1 +1,51 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class AvaCommandListener : MonoBehaviour
+{
+    public GameObject AvaObject;
+    private string lastCommand = "";
+
+    void Start()
+    {
+        StartCoroutine(PollForCommands());
+    }
+
+    IEnumerator PollForCommands()
+    {
+        while (true)
+        {
+            UnityWebRequest www = UnityWebRequest.Get("https://voice-command-handler-242734186757.us-central1.run.app/latest-command");
+            yield return www.SendWebRequest();
+
+            if (!www.isNetworkError && !www.isHttpError)
+            {
+                string command = www.downloadHandler.text.Trim();
+
+                if (command != lastCommand)
+                {
+                    lastCommand = command;
+                    HandleCommand(command);
+                }
+            }
+
+            yield return new WaitForSeconds(3);
+        }
+    }
+
+    void HandleCommand(string command)
+    {
+        Debug.Log("Ava received command: " + command);
+
+        if (command == "show_ava")
+        {
+            AvaObject.SetActive(true);
+        }
+        else if (command == "hide_ava")
+        {
+            AvaObject.SetActive(false);
+        }
+    }
+}
 
