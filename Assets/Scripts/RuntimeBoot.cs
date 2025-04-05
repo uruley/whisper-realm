8fd5f17b186d2af5ee1d545bd5f0085b8e02f59d
uruley <166458385+uruley@users.noreply.github.com>
2025-04-05 15:39:01 -0500

    Update RuntimeBoot.cs

--- Assets/Scripts/RuntimeBoot.cs
+++ Assets/Scripts/RuntimeBoot.cs
@@ -1,1 +1,11 @@
+using UnityEngine;
+
+public class RuntimeBoot : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject loader = new GameObject("RealmLoader");
+        loader.AddComponent<SceneInitializer>();
+    }
+}
 
